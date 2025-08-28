b84b6c97968c40cf8759ec33a456b22ad7a4ad78
sofia-kozak <sonyakozak1@gmail.com>
2025-08-28 18:48:16 +0300

    added task 1 from additional tasks

---
+++ additional tasks(arrays and cycles)/task-1/main.js
@@ -0,0 +1,6 @@
+let numbers = [10, 25, 33, 47, 59];
+let strings = ["яблуко", "банан", "вишня", "груша", "слива"];
+let mixedData = ["текст", 123, true, "hello", false];
+console.log("Числовий масив:", numbers);
+console.log("Рядковий масив:", strings);
+console.log("Змішаний масив:", mixedData);
