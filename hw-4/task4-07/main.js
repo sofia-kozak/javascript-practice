c13b2f5de62196bb28aaa1210f4984e62aaf49b7
sofia-kozak <sonyakozak1@gmail.com>
2025-09-01 19:29:29 +0300

    added task4-07

---
+++ hw-4/task4-07/main.js
@@ -0,0 +1,10 @@
+function foobar(text,counter) {
+    document.write('<ul>')
+    for (let i = 0; i < counter; i++) {
+        document.write(`<li>${text}</li>`)
+    }
+    document.write('</ul>')
+
+}
+
+foobar(`okten`,10)
