0cbbdeff1327c9734ad4b4675f9425ac91fe5f07
sofia-kozak <sonyakozak1@gmail.com>
2025-09-01 19:46:34 +0300

    added task4-13

---
+++ hw-4/task4-13/main.js
@@ -0,0 +1,14 @@
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            return sumUAH / currencyValues[i].value;
+        }
+    }
+}
+
+let result = exchange(
+    10000,
+    [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}],
+    'USD'
+);
+console.log(result);
