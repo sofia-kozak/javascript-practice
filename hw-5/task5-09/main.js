d0f11dba8ef767b4d7bb14ab318e433d8a037d42
sofia-kozak <sonyakozak1@gmail.com>
2025-09-11 18:18:31 +0300

    added task5-09

---
+++ hw-5/task5-09/main.js
@@ -0,0 +1,7 @@
+const foobar = (users) => {
+    for (const user of users) {
+        document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
+    }
+};
+
+foobar([{id: 1, name: 'olha', age: 34}]);
