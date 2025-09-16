7f236abb1c9b58578b788b07fe882d34953ed54d
sofia-kozak <sonyakozak1@gmail.com>
2025-09-16 19:52:21 +0300

    added task6-03

---
+++ hw-6/task6-03/main.js
@@ -0,0 +1,11 @@
+let s1 = 'HELLO WORLD';
+const str1 = s1.toLowerCase();
+console.log(str11);
+
+let s2 = 'LOREM IPSUM';
+const str2 = s2.toLowerCase();
+console.log(str2);
+
+let s3 = 'JAVASCRIPT IS COOL';
+const str3 = s3.toLowerCase();
+console.log(str3);
