031b782e7e322ebc15482f3607786b3826f398b8
sofia-kozak <sonyakozak1@gmail.com>
2025-09-16 19:42:53 +0300

    added task6-01

---
+++ hw-6/task6-01/main.js
@@ -0,0 +1,12 @@
+let str1 = 'hello world';
+let str2 = 'lorem ipsum';
+let str3 = 'javascript is cool';
+
+console.log(str1.length);
+console.log(str2.length);
+console.log(str3.length);
+
+const strings =[str1, str2, str3];
+for (const str of strings) {
+    console.log(str.length)
+}
