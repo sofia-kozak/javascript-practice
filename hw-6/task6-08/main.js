b5274c797227a9ce5c2fac7930a5aaddbfa6768a
sofia-kozak <sonyakozak1@gmail.com>
2025-09-18 18:40:13 +0300

    updated task6-08

--- hw-6/task6-08/main.js
+++ hw-6/task6-08/main.js
@@ -1,5 +1,13 @@
-const numbers = [10,8,-7,55,987,-1011,0,1050,0];
- const strings = numbers.map(number => number + '');
-console.log(strings);
+const coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 
-
+coursesAndDurationArray
+    .sort((a, b) => b.monthDuration - a.monthDuration)
+    .filter(value => value.monthDuration)
+    .map((value, index) => ({...value, id: index + 1}))
