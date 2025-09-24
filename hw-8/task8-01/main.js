3a8e4d0ad0ecd493a8939f60949a918688af1f41
sofia-kozak <sonyakozak1@gmail.com>
2025-09-24 18:09:03 +0300

    added task8-01

---
+++ hw-8/task8-01/main.js
@@ -0,0 +1,10 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+console.log(coursesAndDurationArray.map((course, index) =>({...course, id: index + 1 })));
