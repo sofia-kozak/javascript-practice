1301ec220bbffbe89376a4536c1249eb21dacf75
sofia-kozak <sonyakozak1@gmail.com>
2025-10-06 19:19:13 +0300

    added task9-03

---
+++ hw-9/task9-03/main.js
@@ -0,0 +1,14 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const course of coursesAndDurationArray) {
+    const div = document.createElement('div');
+    div.innerText = `${course.title} ${course.monthDuration}`
+    document.body.appendChild(div);
+}
